c876cdedf95a9f9194a1f3b7e932685d387e4ae3
Harun Sheikhali <harun.sheikhali@agilebits.com>
2022-09-29 09:13:14 -0400

    Added ejs, images, and some html

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const logger = require("./logger");
 
 const app = express();
 
-app.use("/home", (_, response) => {
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use(express.static("pictures"))
+
+app.get("/", function (request, response) {
+	response.render("index");
+})
+
+app.get("/health", (_, response) => {
 	logger.debug("running the logger");
 	return response.send({ message: "alive", status: 200 });
 });
@@ -14,5 +22,5 @@ app.get("/silvia", function (_, response) {
 
 app.listen(3000, () => {
 	console.log("we're off to the races!",
-	"We are live on http://localhost:3100/")
+	"We are live on http://localhost:3000/")
 });
